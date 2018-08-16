34e03997880759c1e31add28c398cefda99bfdcd
Michel Casabianca <casa@sweetohm.net>
2018-08-16 23:52:56 +0200

    Renamed project django_rest

---
+++ README.md
@@ -0,0 +1,4 @@
+Django REST
+===========
+
+This project is a sample Django project to implement a REST API.
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
